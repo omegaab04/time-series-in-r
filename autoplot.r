3e83d583716d6f97f6f730ca39611c89b3453622
Adam B <adam.bouchenaf23@gmail.com>
2025-04-01 13:29:42 +0100

    autoplot

--- autoplot.r
+++ autoplot.r
@@ -1,4 +1,7 @@
 library(ggplot2)
-library(fpp3)
 
-autoplot(maunaloa)
+# Load the dataset from fpp3 or a similar package
+library(fpp3)        # includes mauna_loa
+
+# Plot the time series
+autoplot(mauna_loa)
